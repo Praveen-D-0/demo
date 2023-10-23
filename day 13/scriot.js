b8e72f71e89de85c2a713e5ef7bf3c2db3878808
Praveen-D-0 <jopraveen06@gmail.com>
2023-10-23 17:06:53 +0530

    first commdddit

---
+++ day 13/scriot.js
@@ -0,0 +1,5 @@
+// console.log('javascript works!')
+// console.log(document.getElementById('divElemnt'))
+document.getElementById('divElement').innerText='Revised Content inside the div element'
+let ooh = 
+document.getElementById('dk').innerText="hello world,$(ooh)"
