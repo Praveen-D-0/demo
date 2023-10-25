c3dccd7aebb8de1b7fe890b3c6498a1be7fbe1b7
Praveen-D-0 <jopraveen06@gmail.com>
2023-10-25 11:59:59 +0530

    ljkldfvb

--- day 13/scriot.js
+++ day 13/scriot.js
@@ -1,5 +1,23 @@
-// console.log('javascript works!')
-// console.log(document.getElementById('divElemnt'))
-document.getElementById('divElement').innerText='Revised Content inside the div element'
-let ooh = 
-document.getElementById('dk').innerText="hello world,$(ooh)"
+// // //  create a list with two items 'coffee' and 'tea'
+// // // 1. create a new list item or reference
+// // let list = document.createElement('ul');
+
+// // // 3. Create list items and append it to the list
+// // let listItem1 = document.createElement('li');
+
+// // listItem1.textContent = 'Coffee';
+
+// // let listItem2 = document.createElement('li');
+
+// // listItem2.textContent = 'Tea';
+
+// // list.appendChild(listItem1);
+// // list.appendChild(listItem2);
+
+// let parent = document.createElement('parent')
+
+// document.body.appendChild(list)
+
+for (let index = 0; index <a.length; index++){
+    a[index].style.color = 'blue';
+}
