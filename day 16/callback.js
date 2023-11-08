30ff18c870b822a6ba4fb6f00eb777c3c4d82023
Praveen-D-0 <jopraveen06@gmail.com>
2023-11-08 08:56:13 +0530

    example

---
+++ day 16/callback.js
@@ -0,0 +1,65 @@
+//  let sayHello = function (){
+//     console.log("hello");
+//  }
+//  setTimeout(sayHello, 3000);
+
+// function incrementBy5(N){
+//    return N + 5;
+// }
+// function multiplyBy3(N) {
+//    return N * 3;
+// }
+
+// function arithmeic(N, increment){
+//    N = increment(N);
+//    N = multiplyBy3(N);
+//    console.log(N);
+// }
+// let N = 5;
+// arithmeic(N, incrementBy5);
+// function hello(){
+//    console.log('hello');
+// }
+// let hi = hello;
+// hi();
+//-------------------------------------------------------------
+// function incrementBy5(N){
+//    console.log(N + 5);
+// }
+// let array = [ 1,2,3,4,5];
+// array.forEach(incrementBy5);
+
+// function greet( name, callback) {
+//    console.log(`Hello ${name}!`);
+//    callback();
+
+// }
+// function sayGoodBye(){
+//    console.log('Goodbye!');
+
+// }
+// greet('krish', sayGoodBye);
+
+
+// function greet(name, callback) {
+//     console.log(`Hello ${name}!`);
+//     callback(); // call the callback function
+// }
+
+// function sayGoodBye() {
+//     console.log('Goodbye!');
+// }
+
+// greet('krish', sayGoodBye);
+// function foo() {
+//    console.log("Inside foo");
+//  }
+ 
+//  function bar() {
+//    console.log("Inside bar");
+//    foo();
+//  }
+ 
+//  bar();
+
+ 
